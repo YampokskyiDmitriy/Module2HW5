596392a6945219d45c54edb5430d87ca11de3bf9
Dima <yadima2001@gmail.com>
2021-11-25 16:18:42 +0200

    logger config

---
+++ Module2HW5/Models/LoggerConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Module2HW5.Models
+{
+    public class LoggerConfig
+    {
+        public int FilesLimit { get; set; }
+        public string TimeFormat { get; set; }
+        public string DirectoryPath { get; set; }
+        public string FileExtension { get; set; }
+        public string FileNameFormat { get; set; }
+    }
+}
