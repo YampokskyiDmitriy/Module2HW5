93168797cbd65f965fdb7a6b0f0bcb5559503cfb
Dima <yadima2001@gmail.com>
2021-11-25 16:19:42 +0200

    file service and abstraction

---
+++ Module2HW5/Services/Abstractions/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW5.Services.Abstractions
+{
+    public interface IFileService
+    {
+        public string Read(string path);
+        public IDisposable Create(string path);
+        public void Write(IDisposable streamWriter, string data);
+        public void Close(IDisposable streamWriter);
+    }
+}
