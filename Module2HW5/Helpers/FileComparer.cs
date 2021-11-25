1653ab78fef146bd6aa05fcd20762b3f56c5c86d
Dima <yadima2001@gmail.com>
2021-11-25 16:18:16 +0200

    file comparer

---
+++ Module2HW5/Helpers/FileComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Module2HW5.Helpers
+{
+    public class FileComparer : IComparer
+    {
+        public int Compare(object first, object second)
+        {
+            var firstFile = first as string;
+            var secondFile = second as string;
+
+            if (firstFile == null || secondFile == null)
+            {
+                return 0;
+            }
+
+            var firstFileInfo = new FileInfo(firstFile);
+            var secondFileInfo = new FileInfo(secondFile);
+
+            return DateTime.Compare(firstFileInfo.CreationTime, secondFileInfo.CreationTime);
+        }
+    }
+}
