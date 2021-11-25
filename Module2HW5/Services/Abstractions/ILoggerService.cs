42d60ea42d9fcd02482b3e2ababf28653c14003e
Dima <yadima2001@gmail.com>
2021-11-25 16:19:51 +0200

    logger service and abstraction

---
+++ Module2HW5/Services/Abstractions/ILoggerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW5.Services.Abstractions
+{
+    public interface ILoggerService
+    {
+        public void LogError(string message);
+        public void LogInfo(string message);
+        public void LogWarning(string message);
+        public void SetOutput(string filePath);
+    }
+}
