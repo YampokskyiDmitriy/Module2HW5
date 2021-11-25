334373bd630b0eaf438ddfa59249de0bb0d4c236
Dima <yadima2001@gmail.com>
2021-11-25 16:19:19 +0200

    action service and abstraction

---
+++ Module2HW5/Services/Abstractions/IActionService.cs
@@ -0,0 +1,9 @@
+﻿namespace Module2HW5.Services.Abstractions
+{
+    public interface IActionService
+    {
+        public bool MethodInfo();
+        public bool MethodWarning();
+        public bool MethodError();
+    }
+}
