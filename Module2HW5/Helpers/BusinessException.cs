c1c2d526305a28e01e7b9ed934679168e9386155
Dima <yadima2001@gmail.com>
2021-11-25 16:18:06 +0200

    Business exception

---
+++ Module2HW5/Helpers/BusinessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module2HW5.Helpers
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message)
+        {
+            Message = message;
+        }
+
+        public override string Message { get; }
+    }
+}
